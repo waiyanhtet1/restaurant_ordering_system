11b72c2ef3c1b24e6397f20847bf1a8a5ff0e36d
Wai_Yan_Htet <waiyanhtet1661390@gmail.com>
2022-01-26 22:43:23 +0630

    authsetup adminltesetup

---
+++ resources/views/kitchen/dish.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.master')
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dishes Panel</h1>
+          </div><!-- /.col -->
+          {{-- <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col --> --}}
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+  
+    <!-- Main content -->
+    <div class="content">
+      <div class="card">
+          <div class="card-body">
+                <table id="example1" class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Column 1</th>
+                            <th>Column 2</th>
+                            <th>Column 3</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Row 1 Data 1</td>
+                            <td>Row 1 Data 2</td>
+                            <td>Row 1 Data 2</td>
+                        </tr>
+                        <tr>
+                            <td>Row 2 Data 1</td>
+                            <td>Row 2 Data 2</td>
+                            <td>Row 2 Data 2</td>
+                        </tr>
+                        <tr>
+                            <td>Row 2 Data 1</td>
+                            <td>Row 2 Data 1</td>
+                            <td>Row 2 Data 2</td>
+                        </tr>
+                    </tbody>
+                </table>
+          </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false,
+    'confim' => false,
+]);
+
+Route::get('/order', [App\Http\Controllers\OrderController::class, 'index'])->name('home');
+
+Route::resource('/dish',App\Http\Controllers\DishController::class);
